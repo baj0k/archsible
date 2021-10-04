743da5df185a8af0eac27d68d102a808cdda8637
bajok <bajok@kretes.xyz>
2021-10-04 21:15:05 +0200

    change for local execution

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#help() {
+#    echo "Usage: ./install [-p arg] [-w arg] [-d arg] [-g arg]"
+#    echo "-p (the password for the private mail account)"
+#    echo "-d (the password for the cal- and carddav account)"
+#    echo "-g (the password for the github account)"
+#    echo "-h (print this message)"
+#}
+#
+#while getopts ':p:w:d:g:h' opt; do
+#    case $opt in
+#        p)
+#            private_pw=$OPTARG
+#            ;;
+#        d)
+#            dav_pw=$OPTARG
+#            ;;
+#        g)
+#            github_pw=$OPTARG
+#            ;;
+#        h)
+#            help
+#            exit
+#            ;;
+#        ?)
+#            echo "Invalid Parameter given. $(help)"
+#            ;;
+#    esac
+#done
+
+git pull
+ansible-playbook -i hosts roles.yml -vv
