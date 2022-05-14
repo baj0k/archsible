996c41490ce7dfa39c8fb0ba55459bc0fb91b091
bajok <bajok@kretes.xyz>
2022-05-14 11:44:04 +0200

    Remove pacman-aria2dl service and timer as it was integrated to statusbar scripts

--- setup-vm.sh
+++ setup-vm.sh
@@ -4,11 +4,11 @@ LIBVIRT_DIR="${XDG_DATA_HOME:-/usr/share}/libvirt"
 RELEASE="2022.04.05"
 
 # Download Arch Linux ISO if not already present on the system
-[ ! -e "${LIBVIRT_DIR}/iso/archlinux-${RELEASE}-x86_64.iso" ] && echo "Downloading Arch Linux ISO"
-/usr/bin/aria2c -q --seed-time=0 --follow-torrent=mem --dir="${LIBVIRT_DIR}/iso" --on-download-complete="/bin/true" "https://archlinux.org/releng/releases/${RELEASE}/torrent/" || true
+[ ! -e "${LIBVIRT_DIR}/iso/archlinux-${RELEASE}-x86_64.iso" ] && echo "Downloading Arch Linux ISO" || exit 0
+aria2c -q --seed-time=0 --follow-torrent=mem --dir="${LIBVIRT_DIR}/iso" --on-download-complete="/bin/true" "https://archlinux.org/releng/releases/${RELEASE}/torrent/" || true
 
 # Create VM
-/usr/bin/virt-install -q \
+virt-install -q \
 --name="${1:-archsible}" \
 --vcpus=4 \
 --memory=4096 \
@@ -17,26 +17,29 @@ RELEASE="2022.04.05"
 --os-variant=archlinux \
 --graphics spice \
 --boot uefi \
---wait=1 & /usr/bin/sleep 5
+--wait=1 & sleep 5
+virt-manager --connect qemu:///system --show-domain-console "${1:-archsible}"
 
-echo "Wait for the installation medium to boot and follow the steps from usage section in README. Ansible playbooks will be executed automatically by this script. When you're ready press any key to continue." && read -r
-
-# Run installation play and create snapshot if successful
+# Run installation play
+echo "Wait for the installation medium to boot and follow the steps from usage section in README.md. When you're ready press any key to execute the installation play."; read -r REPLY
 echo "Provide guest system root password to continue."
-/usr/bin/ansible-playbook archsible.yml --limit=vms --tags install || exit 0 
-[ "$(/usr/bin/virsh list --all | /usr/bin/grep "${1:-archsible}" | /usr/bin/awk '{ print $3}' || true)" != "shut" ] &&
-/usr/bin/virsh shutdown --domain "${1:-archsible}" && /usr/bin/sleep 5
-/usr/bin/virsh snapshot-create-as --domain "${1:-archsible}" --name "install" &&
-/usr/bin/virsh start --domain "${1:-archsible}"
 
-# Run post-installation play and create snapshot if successful
+ansible-playbook archsible.yml --limit=vms --tags install || exit 0 
+[ "$(virsh list --all | grep "${1:-archsible}" | awk '{ print $3}' || true)" != "shut" ] &&
+virsh shutdown --domain "${1:-archsible}" && sleep 5
+virsh snapshot-create-as --domain "${1:-archsible}" --name "install" &&
+virsh start --domain "${1:-archsible}"
+
+# Run post-installation play
+echo "wait for the machine to reboot. Depending on the network configuration of the hypervisor you may need to change the machine's ip address in the inventory file. To avoid errors regarding host key checking remove your ~/.ssh/known_hosts file. When you're ready press any key to execute the post-installation play."; read -r REPLY
 echo "Provide guest system user password to continue."
-/usr/bin/ansible-playbook archsible.yml --limit=vms --skip-tags install || exit 0
-[ "$(/usr/bin/virsh list --all | /usr/bin/grep "${1:-archsible}" | /usr/bin/awk '{ print $3}' || true)" != "shut" ] &&
-/usr/bin/virsh shutdown --domain "${1:-archsible}" && /usr/bin/sleep 5
-/usr/bin/virsh snapshot-create-as --domain "${1:-archsible}" --name "post-install" &&
-/usr/bin/virsh start --domain "${1:-archsible}"
+
+ansible-playbook archsible.yml --limit=vms --skip-tags install || exit 0
+[ "$(virsh list --all | grep "${1:-archsible}" | awk '{ print $3}' || true)" != "shut" ] &&
+virsh shutdown --domain "${1:-archsible}" && sleep 5
+virsh snapshot-create-as --domain "${1:-archsible}" --name "post-install" &&
+virsh start --domain "${1:-archsible}"
 
 # Connect to VM
-/usr/bin/killall /usr/bin/virt-manager && 
-/usr/bin/sleep 5 && /usr/bin/virt-manager --connect qemu:///system --show-domain-console "${1:-archsible}"
+killall virt-manager && 
+sleep 5 && virt-manager --connect qemu:///system --show-domain-console "${1:-archsible}"
